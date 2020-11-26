6f615838754c655d0ce510e354433ca7a61a33e1
Ivens Del Angel <idelangel@centrisinfo.com>
2020-11-26 11:33:33 -0600

    Addin new php file for handling the SDK Fix 2

--- detail.php
+++ detail.php
@@ -104,8 +104,9 @@
                                         <input type="hidden" name="title" value="<?php echo $_POST['title'] ?>">
                                         <input type="hidden" name="price" value="<?php echo $_POST['price'] ?>">
                                         <input type="hidden" name="unit" value="<?php echo $_POST['unit'] ?>">
+                                        <button type="submit"  class="mercadopago-button">Pagar</button>
                                     </form>                        
-                                    <button type="submit"  class="mercadopago-button">Pagar</button>
+                                    
                                 </div>
                             </div>
                         </div>
